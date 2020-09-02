fda146c252d3b4ef2ce4b1179753509b5d496231
sneheshdutta <sneheshdutta@gmail.com>
2020-09-02 22:57:51 +0530

    one code added

---
+++ avg corona test1.py
@@ -0,0 +1,11 @@
+print("Go Corona Go")
+
+num1=input("Enter the number of cases in your area")
+num2=input("Enter the number of cases in your area")
+num3=input("Enter the number of cases in your area")
+num4=input("Enter the number of cases in your area")
+num5=input("Enter the number of cases in your area")
+
+sum= float(num1) + float(num2) + float(num3) + float(num4) + float(num5)
+avg = sum/5
+print("Avarage",avg)
