789937eba2959746341bc4ce25903a07500fa2ed
sneheshdutta <sneheshdutta@gmail.com>
2020-09-03 12:46:54 +0530

    new code added

---
+++ avg corona test2.py
@@ -0,0 +1,11 @@
+print("Go Corona Go")
+
+count =0
+sum=0
+while count < 5:
+  num = input("Enter the numbers of cases in your area")
+  count = count+1
+  sum= sum + float(num)
+
+avg = sum/5
+print("Avarage",avg)
